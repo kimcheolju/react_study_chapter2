7cdd211f287faf3947cc24b61b322f1d5c642e74
Baruce <kimcj020828@gmail.com>
2021-03-14 23:46:51 +0900

    8.4 useMemo까지 완료

--- src/App.js
+++ src/App.js
@@ -1,33 +1,12 @@
+import logo from './logo.svg';
 import './App.css';
-import { Component, Fragment } from 'react';
-import LifeCycleSample from './LifeCycleSample';
-import ErrorBoundary from './ErrorBoundary';
+import { useState } from 'react';
+import Average from './Average';
 
-function getRandomColor () {
-  return '#' + Math.floor(Math.random() * 16777215).toString(16);
-}
 
-class App extends Component{
-  state =  {
-    color: '#000000'
-  }
-
-  handleClick = () => {
-    this.setState({
-      color: getRandomColor()
-    })
-  }
-
-  render() {
-    return (
-      <div>
-        <button onClick={this.handleClick}>랜덤 색상</button>
-        <ErrorBoundary>
-          <LifeCycleSample color={this.state.color} />
-        </ErrorBoundary>
-      </div>
-    )
-  }
+const App = () => {
+  
+  return <Average />
 }
 
 export default App;
--- src/Counter.js
+++ src/Counter.js
@@ -1,35 +1,29 @@
-import React, { Component } from 'react'
+import React, { useReducer, useState } from 'react';
 
-class Counter extends Component {
-    state = {
-        number: 0,
-        fixedNumber: 0
+function reducer(state, action) {
+    // action.type에 따라 다른 작업 수행
+    switch (action.type) {
+        case 'INCREAMENT':
+            return {value: state.value + 1};
+        case 'DECREAMENT':
+            return {value:state.value -1};
+        default:
+            // 아무것도 해당되지 않으면 기존상태 반영
+            return state;
     }
-
-    render() {
-        const {number, fixedNumber} = this.state; // state를 조회할 때는 this.state로 조회합니다.
-        return (
-            <div>
-                <h1>{number}</h1>
-                <h2>바뀌지 않는 값:{fixedNumber}</h2>
-                <button
-                // onclick을 통해 버튼이 클릭되었을 때 호출할 함수를 지정
-                onClick={()=>{
-                    //this.setState를 사용하여 state에 새로운 값을 넣을 수 있음
-                    this.setState({
-                        number: number + 1
-                    }, ()=>{
-                        console.log('조금 전 setState가 실행되었습니다.');
-                        console.log(this.state);
-                    })
-                }}
-                >
-                  + 1  
-                </button>
-            </div>
-        )
-    }
-    
 }
+
+const Counter = () => {
+    const [state, dispatch] = useReducer(reducer, {value: 0});
+    return (
+        <div>
+            <p>
+                현재 카운터 값은 <b> { state.value } 입니다.</b>
+            </p>
+            <button onClick={()=>dispatch({type: 'INCREAMENT'})}>+1</button>
+            <button onClick={()=>dispatch({type: 'DECREAMENT'})}>-1</button>
+        </div>
+    );
+};
 
 export default Counter;
